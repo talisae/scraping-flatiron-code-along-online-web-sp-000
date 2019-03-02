b225ed698be08e81b89e6b2ba753f3d60defddca
Talisa Erazo <talisaerazo@gmail.com>
2019-03-02 02:16:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,17 @@
 equire 'nokogiri'
 require 'open-uri'
 require 'pry'
- 
+
 require_relative './course.rb'
- 
+
 class Scraper
- 
+
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
- 
+
       binding.pry
   end
- 
+
 end
- 
+
 Scraper.new.get_page
